b818298f776c5a7d2bc4d9ee497241ce3b763fd6
Phaneendra79 <155842268+Phaneendra76@users.noreply.github.com>
2024-01-10 12:37:29 +0530

    Just adding the one class

---
+++ src/Dont_Touch_me/Alertboxes.java
@@ -0,0 +1,14 @@
+package Dont_Touch_me;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alertboxes {
+
+	public static void main(String[] args)
+	{
+       WebDriver driver=new ChromeDriver();
+       driver.get("https://demo.automationtesting.in/Alerts.html");
+	}
+
+}
