c4fefc7ebba0b01a432c0143a348e9dd44d68698
Phaneendra76 <155842268+Phaneendra76@users.noreply.github.com>
2024-01-10 11:40:43 +0530

    update the for loop 1-10
     update the for loop 
    
    printing the numbers for 1 to10

--- src/Dont_Touch_me/Marry_me.java
+++ src/Dont_Touch_me/Marry_me.java
@@ -3,21 +3,27 @@ package Dont_Touch_me;
 public class Marry_me 
 {
  public static void main(String[] args) {
-		        int proposalStatus = 1; 
+		        // int proposalStatus = 1; 
 
-		        switch (proposalStatus) {
-		            case 1:
-		                System.out.println("Will you marry me? bulli");
-		                break;
-		            default:
-		                System.out.println("Unexpected proposal status");
-		        }
+		        // switch (proposalStatus) {
+		        //     case 1:
+		        //         System.out.println("Will you marry me? bulli");
+		        //         break;
+		        //     default:
+		        //         System.out.println("Unexpected proposal status");
+		        // }
 
 		       
-		        System.out.println("Love is not just something. It's everything. ❤");
-		        System.out.println("You had me at 'Ammu.' 💑");
-		        System.out.println("In your smile, I see something more beautiful than the stars. 🌟");
-	                System.out.println("Phani");
+		        // System.out.println("Love is not just something. It's everything. ❤");
+		        // System.out.println("You had me at 'Ammu.' 💑");
+		        // System.out.println("In your smile, I see something more beautiful than the stars. 🌟");
+	         //        System.out.println("Phani");
+
+	 for(int i=1;i<=10;i++)
+		 {
+                      System.out.println(i);
+		 }
+	 
 		    }
 		}
 
